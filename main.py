7e38f7f6340ff3547f80940124f56558cfff8339
phliem <philipp.liehm@gmail.com>
2023-09-23 17:52:52 +0200

    adding text to an image

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ GUI content:
 
 import tkinter 
 from tkinter import filedialog as fd
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageFont, ImageDraw
 import time
+
+font = ImageFont.truetype("arial.ttf", 100)
 
 filename = ""
 def open_file():
@@ -26,20 +28,28 @@ def open_file():
     filename = fd.askopenfilename()
     print(filename)
     photo = ImageTk.PhotoImage(file=filename)
-    canvas.delete("all") # deletes everything showing on the canvas
-    canvas.create_image(100,100,image = photo)
+    canvas.delete("all") # deletes everything showing on the canvas'
+    canvas.create_image(200,200,image = photo)
     canvas.grid(column=0,row=0)
     canvas.image = photo # if this is not done, it wont work, no image will be shown, ChatGPT says its because the image gets garbage collected, not sure why that is the case
     
 
+def show_watermark():
+    im = Image.new(mode="RGB", size=(800, 800))
+    I1 = ImageDraw.Draw(im)
+    I1.text((50, 400), "Watermark", font=font, fill =(50, 50, 50))
+    im.show()
+
 
 window = tkinter.Tk() # initialise the tkinter window
-test_image = ImageTk.PhotoImage(file="D:/Coding/1_LearnPython/1_100Days_of_Python/38_Render_Html_webpage/static/images/pic09.jpg")
 window.title("Watermarker")
-canvas = tkinter.Canvas(width=800, height=800)
-canvas.create_image(500,200,image=test_image)
+window.config(padx=50, pady=50)
+canvas = tkinter.Canvas(width=800, height=800, bg='black')
 canvas.grid(column=0,row=0)
 
 b_load = tkinter.Button(text="Load image", command=open_file)
 b_load.grid(column=3, row=3)
+
+b_show_water_image = tkinter.Button(text="Show Watermark", command=show_watermark)
+b_show_water_image.grid(column=0, row=3)
 window.mainloop() # run the mainloop, keep the window open
