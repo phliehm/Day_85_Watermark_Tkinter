f56633e4a00e6fb56d67df1f8446e77623721b04
phliem <philipp.liehm@gmail.com>
2023-09-24 15:28:22 +0200

    Works, but changes size

--- main.py
+++ main.py
@@ -17,28 +17,29 @@ GUI content:
 
 import tkinter 
 from tkinter import filedialog as fd
-from PIL import Image, ImageTk, ImageFont, ImageDraw
+from PIL import Image, ImageTk, ImageFont, ImageDraw, ImageGrab
 import time
 import math
 
 
 
+
 filename = ""
 def open_file():
+    print(canvas.winfo_height(), canvas.winfo_width())
     global filename
     filename = fd.askopenfilename()
     print(filename)
     photo = ImageTk.PhotoImage(file=filename)
     canvas.config(width=photo.width(),height=photo.height())
     canvas.delete("all") # deletes everything showing on the canvas'
-    canvas.create_image(photo.width()/2+1,photo.height()/2+1,image = photo)
+    canvas.create_image(photo.width()/2,photo.height()/2,image = photo)
     canvas.grid(column=0,row=0)
     canvas.image = photo # if this is not done, it wont work, no image will be shown, ChatGPT says its because the image gets garbage collected, not sure why that is the case
 
-  
 
-
-def add_2_images():
+def add_watermark():
+    print(canvas.winfo_height(), canvas.winfo_width())
     global filename
     
     n = 5 # number of watermarks
@@ -65,32 +66,40 @@ def add_2_images():
         combined = Image.alpha_composite(base,txt_image)
         canvas.delete("all") # deletes everything showing on the canvas'
         photo = ImageTk.PhotoImage(combined)
-        canvas.create_image(photo.width()/2 + 1,photo.height()/2 + 1,image = photo)
+        canvas.create_image(photo.width()/2 ,photo.height()/2,image = photo, anchor='center')
         canvas.grid(column=0,row=0)
         canvas.image = photo
         #combined.show()
 
 
-# 1. 10% from edge away
-# 2. calculate font size
-# 3. calculate how many lines possible
-# 4. draw the text multiple times always shifted a bit to the right so it ends up 10% from the right    
-
+def save_image():
+    x0 = canvas.winfo_rootx()
+    y0 = canvas.winfo_rooty()
+    x1 = x0 + canvas.winfo_width()
+    y1 = y0 + canvas.winfo_height()
+    print(f"Width: {canvas.winfo_width()}, Height: {canvas.winfo_height()}")
+    img = ImageGrab.grab(bbox= (x0,y0,x1,y1))
+    img.save("Result.png")
+    
 window = tkinter.Tk() # initialise the tkinter window
 window.title("Watermarker")
 window.config(padx=50, pady=50)
 canvas = tkinter.Canvas(width=800, height=800, bg='black')
-canvas.grid(column=0,row=0, columnspan=3)
+canvas.grid(column=0,row=0, columnspan=4)
 
-b_load = tkinter.Button(text="Load image", command=open_file)
-b_load.grid(column=3, row=3)
 
-b_add_2_images = tkinter.Button(text="Apply watermark", command= add_2_images)
-b_add_2_images.grid(column=1,row=3)
+b_load = tkinter.Button(text="1. Load image", command=open_file)
+b_load.grid(column=0, row=3, pady=10, sticky='W')
 
-l_watermark = tkinter.Label(text="Watermark text:")
-l_watermark.grid(column=0, row=3, sticky='W')
-entry_watermark = tkinter.Entry()
-entry_watermark.grid(column=0, row=3, sticky='E')
+b_add_watermarks = tkinter.Button(text="3. Apply watermark", command= add_watermark)
+b_add_watermarks.grid(column=2,row=3)
+
+l_watermark = tkinter.Label(text="2. Watermark text:")
+l_watermark.grid(column=1, row=3, sticky='W')
+entry_watermark = tkinter.Entry(width=15)
+entry_watermark.grid(column=1, row=3, sticky='E')
+
+b_save_new_image = tkinter.Button(text="4. Save new image", command=save_image)
+b_save_new_image.grid(column=3, row=3, sticky='E')
 
 window.mainloop() # run the mainloop, keep the window open
